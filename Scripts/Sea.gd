75df2b2027f823d1c57a4aed759737aa6dd560f3
MarianPoirot <81marianp@gmail.com>
2024-03-09 15:34:00 +0100

    Infinite sea but stupid wave speed

--- Scripts/Sea.gd
+++ Scripts/Sea.gd
@@ -2,31 +2,39 @@ extends StaticBody2D
 
 var t:float = 0
 var dt:float = 4.0
+var Ship
+var ship_position_x
 
 var noise:FastNoiseLite = FastNoiseLite.new()
 
 func _ready():
 	noise.seed = randi()
 	noise.fractal_octaves = 4
+	Ship = get_parent().get_node('Ship')
 
 func _process(delta):
+	ship_position_x = Ship.global_position.x
 	var vertices:Array = []
 	# the first two are bottom right and bottom left vertices:
 	vertices.append(Vector2(1000, 100))
 	vertices.append(Vector2(-1000, 100))
 	for i in range(-100, 101):
 		# make a sum of sin to generate some sort of wave
-		var y:float = 3 * sin(i - t)
-		y += 0.5 * sin(1.8 * i - 4.1 * t)
-		y += 4 * sin(0.1 * i - 0.5 * t)
+		var y:float = 35 * sin(0.05 * i - 0.05 * t)
+		y += 3 * sin(0.09 * i - 0.21 * t)
+		y += 20 * sin(0.05 * i - 0.25 * t)
 		# add some noise to make it look a bit random
-		y += 10 * noise.get_noise_1d(10 * i - t);
+		y += 5 * noise.get_noise_1d(5 * i - 0.5 * t);
 		# the position have to match the width of the polygon
 		# here 100 * 10 = 1000, see first two vertices above
 		vertices.append(Vector2(i * 10, y))
 	
 	# update the polygons
+	$DynamicWaves.global_position.x = ship_position_x -300
 	$DynamicWaves.polygon = PackedVector2Array(vertices)
+	$DynamicShape.global_position.x = ship_position_x - 300
 	$DynamicShape.polygon = PackedVector2Array(vertices)
+	print("ship_position_x = ",ship_position_x,"; DynamicWaves = ", $DynamicWaves.position.x,"; DynamicShape = ",$DynamicShape.position.x)
+	
 
 	t += dt * delta # make the waves move over time
