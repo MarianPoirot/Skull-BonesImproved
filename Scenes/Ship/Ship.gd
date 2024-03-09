4a8d1ac475e3ded59496a8a69714c30b5c70378e
Vallet <baba18za@gmail.com>
2024-03-09 11:24:17 +0100

    Added water level system linked to the game over

--- Scenes/Ship/Ship.gd
+++ Scenes/Ship/Ship.gd
@@ -1,5 +1,9 @@
 extends RigidBody2D
 
+var boatSpeed = 100
+var rotationSpeed = 10
+var maxWaterLevel = 200
+var currentWaterLevel = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +13,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("SteerLeft"):
-		angular_velocity += delta * 10
+		angular_velocity += delta * rotationSpeed
 	if Input.is_action_pressed("SteerRight"):
-		angular_velocity -= delta * 10
+		angular_velocity -= delta * rotationSpeed
+	if Input.is_action_pressed("Sink"):
+		AddWater(1)
+
+func AddWater(amount):
+	currentWaterLevel += amount
+	if(currentWaterLevel >= maxWaterLevel):
+		_OnLose()
+
+func _OnLose():
+	if get_tree().change_scene_to_file("res://Scenes/UI/Ending.tscn") != OK:
+		print ("Error passing from Opening scene to main scene")
