2c6768e68299cf37f32e5f370a6a04617d6aa82f
MarianPoirot <81marianp@gmail.com>
2024-03-09 22:02:07 +0100

    removed deploy sail

--- Scripts/Ship/Sail.gd
+++ Scripts/Ship/Sail.gd
@@ -7,7 +7,6 @@ var windForceFromLevel
 var windForceRatio
 var windOrientationFromLevel
 var sailOrientation
-var sailDeploy
 var sailLife
 var windForceBeforeDamage = 10
 var deployed : bool
@@ -23,7 +22,6 @@ func _ready():
 	windOrientationFromLevel = 0
 	sailOrientation = 0
 	windForceBeforeDamage = 10
-	sailDeploy = 1
 	sailLife = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame. 
@@ -40,23 +38,13 @@ func _process(delta):
 		if sailOrientation <= - PI:
 			sailOrientation += 2* PI
 	
-	if Input.is_action_pressed("DeploySail"):
-		sailDeploy -= 0.2 * delta
-		if sailDeploy < 0 :
-			sailDeploy = 0
-	if Input.is_action_pressed("PullSail"):
-		sailDeploy += 0.2 * delta
-		if sailDeploy > 1:
-			sailDeploy = 1
-	
 	windForceRatio = 0.5 + cos(windOrientationFromLevel - sailOrientation)/2
-	var wind = windForceRatio * windForceFromLevel * sailDeploy
+	var wind = windForceRatio * windForceFromLevel
 	
 	if wind >= windForceBeforeDamage:
 		sailLife -=0.01 * delta
 
 	var text = "wind orientation "+ str(sailOrientation).substr(0,4) + "/" + str(windOrientationFromLevel) + "\n"
-	text = text + "sail deploy" + str(sailDeploy) + "\n"
 	text = text + "wind force" + str(wind) + "\n"
 	text = text + "wind force Ratio" + str(windForceRatio) + "\n"
 	$WindLabel.text = text
