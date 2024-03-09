1a6264afc0e4569aa0679ba35aa8a16717db62e5
MarianPoirot <81marianp@gmail.com>
2024-03-09 16:52:13 +0100

    Changed direction of waves

--- Scripts/Sea.gd
+++ Scripts/Sea.gd
@@ -14,17 +14,18 @@ func _ready():
 
 func _process(delta):
 	ship_position_x = Ship.global_position.x
+	
 	var vertices:Array = []
 	# the first two are bottom right and bottom left vertices:
 	vertices.append(Vector2(1000, 100))
 	vertices.append(Vector2(-1000, 100))
 	for i in range(-100, 101):
 		# make a sum of sin to generate some sort of wave
-		var y:float = 35 * sin(0.05 * i - 0.05 * t)
-		y += 3 * sin(0.09 * i - 0.21 * t)
-		y += 20 * sin(0.05 * i - 0.25 * t)
+		var y:float = 35 * sin(0.05 * i + 0.05 * t)
+		y += 3 * sin(0.09 * i + 0.21 * t)
+		y += 20 * sin(0.05 * i + 0.25 * t)
 		# add some noise to make it look a bit random
-		y += 5 * noise.get_noise_1d(5 * i - 0.5 * t);
+		y += 5 * noise.get_noise_1d(5 * i + 0.5 * t);
 		# the position have to match the width of the polygon
 		# here 100 * 10 = 1000, see first two vertices above
 		vertices.append(Vector2(i * 10, y))
