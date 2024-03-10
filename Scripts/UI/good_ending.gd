d4aa6d65c10c0ec80d962b418161ffb1ba0d887d
Vallet <baba18za@gmail.com>
2024-03-10 16:23:02 +0100

    Update : implemented the end island

---
+++ Scripts/UI/good_ending.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("start"):
+		_on_restart_button_button_down()
+	if Input.is_action_pressed("quit"):
+		_on_close_button_button_down()
+
+func _on_restart_button_button_down():
+	if get_tree().change_scene_to_file("res://Scenes/UI/Main.tscn") != OK:
+		print ("Error passing from Opening scene to main scene")
+
+func _on_close_button_button_down():
+	get_tree().quit()
