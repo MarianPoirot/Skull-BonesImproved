105f6c52b62ea8628553bfff763f6cc20ff8fe28
Lilian Hiault <lilian.hiault@laposte.net>
2024-03-10 12:57:47 +0100

    Corrected some warnings

--- Scripts/Lightning.gd
+++ Scripts/Lightning.gd
@@ -1,16 +1,5 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func lightning():
 	$AudioStreamPlayer2D.play()
 	$PointLight2D.energy = 1
