58991ac790ff130ba93799e863faf9fc16414216
MarianPoirot <81marianp@gmail.com>
2024-03-10 09:53:41 +0100

    Sail is well destroyed and repaired

--- Scripts/UI/wind_compass.gd
+++ Scripts/UI/wind_compass.gd
@@ -1,35 +1,25 @@
 extends Node2D
 
-var windArrow
-var windOrientationGoal
-var currentWindOrientation
-
-var sailOrientationGoal
-var currentSailOrientation
+@export var windOrientationGoal = 0
+@export var currentWindOrientation = -0.5
+@export var sailOrientationGoal = 1
+@export var currentSailOrientation = 0.5
 var sailArrow
+var windArrow
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	currentWindOrientation = -0.5
-	windOrientationGoal = 0
-	sailOrientationGoal = 1
-	currentSailOrientation = 0.5
-	windArrow = $WindArrow.create_tween()
-	sailArrow = $SailArrow.create_tween()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	windArrow = $WindArrow.create_tween()
 	sailArrow = $SailArrow.create_tween()
 	windArrow.tween_property($WindArrow, "rotation", windOrientationGoal, delta)
 	sailArrow.tween_property($SailArrow, "rotation", sailOrientationGoal, delta)
 
-
 func _on_main_wind_changes(_windForce, windOrientation):
 	windOrientationGoal = windOrientation
 
-
 func _on_ship_sail_orientation_from_ship(orientation):
-	print("Compass" + str(orientation))
 	sailOrientationGoal = orientation
