8c2fd18800776cdebe939ac2708fcae6b30b3644
Vallet <baba18za@gmail.com>
2024-03-10 14:37:52 +0100

    Detection is correctly made, but not the appeareance neither the spawn of the new object

--- Scripts/Obstacles/Obstacle.gd
+++ Scripts/Obstacles/Obstacle.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
 @export var ExplodingObstacle : PackedScene
-
+var michel : bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	michel = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_area_2d_body_entered(body):
-	$Area2D/CollisionShape2D.set_deferred("disabled", true)
-	add_child(ExplodingObstacle.instantiate())
+func _on_hitbox_body_entered(body):
+	if michel:
+		remove_child(get_node("RigidBody2D"))
+		add_child(ExplodingObstacle.instantiate())
+		michel = false
